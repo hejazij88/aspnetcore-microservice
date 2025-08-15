4925d8cf91d9baa156f0111845fa190cdaea1fed
98913 <98913@LAPTOP-JQR66TAF>
2025-08-15 10:56:11 +0330

    Add Redis Connection

--- Services/Basket/Basket.Api/Program.cs
+++ Services/Basket/Basket.Api/Program.cs
@@ -3,6 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+//redis Connection
+builder.Services.AddStackExchangeRedisCache(option =>
+{
+    option.Configuration = builder.Configuration.GetValue<string>("CacheSetting:ConnectionString");
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
