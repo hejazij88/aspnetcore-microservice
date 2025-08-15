fde503fe69dd745d755eeaca2b1684bc19150d7d
98913 <98913@LAPTOP-JQR66TAF>
2025-08-15 11:12:19 +0330

    Add Basket Entitys

---
+++ Services/Basket/Basket.Api/Entitys/Cart.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.Api.Entitys;
+
+public class Cart
+{
+    public string UserName { get; set; }
+
+    public List<Items> ItemsList { get; set; }
+
+    public decimal TotalPrice
+    {
+        get
+        {
+            decimal total = 0;
+            foreach (var item in ItemsList)
+            {
+                total += item.Price*item.Count;
+            }
+            return total;
+        }
+    }
+}
---
+++ Services/Basket/Basket.Api/Entitys/Items.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.Api.Entitys;
+
+public class Items
+{
+    public string ProductId { get; set; }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
+    public int Count { get; set; }
+
+}
