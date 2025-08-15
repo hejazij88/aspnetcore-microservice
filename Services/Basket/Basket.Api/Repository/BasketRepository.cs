31fc60c6525092d69718773b2fcc14b75448486d
98913 <98913@LAPTOP-JQR66TAF>
2025-08-15 11:33:42 +0330

    Add Basket Repository

---
+++ Services/Basket/Basket.Api/Repository/BasketRepository.cs
@@ -0,0 +1,34 @@
+﻿using Basket.Api.Entitys;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.Api.Repository;
+
+public class BasketRepository:IBasketRepository
+{
+    private readonly IDistributedCache _redis;
+
+    public BasketRepository(IDistributedCache redis)
+    {
+        _redis = redis;
+    }
+
+    public async Task<Cart?> GetCartAsync(string userName)
+    {
+        var basket= await _redis.GetStringAsync(userName);
+        if (basket == null) return null;
+        return JsonConvert.DeserializeObject<Cart>(basket);
+    }
+
+    public async Task<Cart?> UpdateCartAsync(Cart cart)
+    {
+       await _redis.SetStringAsync(cart.UserName, JsonConvert.SerializeObject(cart));
+       return await GetCartAsync(cart.UserName);
+
+    }
+
+    public async Task DeleteCartAsync(string userName)
+    {
+         await _redis.RemoveAsync(userName);
+    }
+}
---
+++ Services/Basket/Basket.Api/Repository/IBasketRepository.cs
@@ -0,0 +1,10 @@
+﻿using Basket.Api.Entitys;
+
+namespace Basket.Api.Repository;
+
+public interface IBasketRepository
+{
+    Task<Cart?> GetCartAsync(string userName);
+    Task<Cart?> UpdateCartAsync(Cart cart);
+    Task DeleteCartAsync(string userName);
+}
